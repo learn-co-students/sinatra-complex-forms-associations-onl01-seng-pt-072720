0fa8b97d60cf01e823710837d9cd51e5f07b0f3d
cinna-codes <somethingfellipickeditup@gmail.com>
2020-10-29 21:08:21 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -23,15 +23,28 @@ class PetsController < ApplicationController
     redirect to "/pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owner = @pet.owner
+    erb :'/pets/edit'
+  end
+
   get '/pets/:id' do 
     # binding.pry
     @pet = Pet.find(params[:id])
-    @owner = @pet.owner
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
+
+    @pet.update(params)
+
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(params[:owner])
+      @pet.owner = @owner
+    end
+
     redirect to "/pets/#{@pet.id}"
   end
 end
