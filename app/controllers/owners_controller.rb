651fa5ae8da1465218b52a46d2673c6034da78b6
Your Username <you@example.com>
2020-11-01 23:37:45 -0500

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -34,6 +34,7 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do
+    # bug fix
     if !params[:owner].keys.include?("pet_ids")
       params[:owner]["pet_ids"] = []
     end
